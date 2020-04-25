b6eed372aa66ce4da946082dcd8a3d86d64cd719
FrancoisBasset <>
2020-04-25 19:09:19 +0200

    mainapi: Swagger
    Ajout de swagger-ui-express
    Documentation de domains

--- mainapi/index.js
+++ mainapi/index.js
@@ -1,1 +1,11 @@
-console.log('It works');
+const express = require('express');
+const app = express();
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('yamljs').load('./swagger.yml');
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(80, function() {
+	console.log('Start on 80');
+});
