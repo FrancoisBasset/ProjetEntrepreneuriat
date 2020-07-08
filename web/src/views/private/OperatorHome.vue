b75124fa711f21637785671b3f7b36d1ad5c00a9
FrancoisBasset <>
2020-07-08 18:43:16 +0200

    OperatorHome

--- web/src/views/private/OperatorHome.vue
+++ web/src/views/private/OperatorHome.vue
@@ -1,26 +1,28 @@
 <template>
 	<div>
 		<HomeBar />
-
-		<router-link :to="{ name: 'operatorUsers' }">
-			<button>Gestion des utilisateurs</button>
-		</router-link>
-
-		<router-link :to="{ name: 'operatorPayments' }">
-			<button>Gestion des paiements</button>
-		</router-link>
 		
-		<router-link :to="{ name: 'operatorCommunications' }">
-			<button>Gestion des communications</button>
-		</router-link>
+		<div id="buttons">
+			<router-link :to="{ name: 'operatorUsers' }">
+				<button class="button">Gestion des utilisateurs</button>
+			</router-link>
 
-		<router-link :to="{ name: 'operatorFormations' }">
-			<button>Gestion des formations</button>
-		</router-link>
+			<router-link :to="{ name: 'operatorPayments' }">
+				<button class="button">Gestion des paiements</button>
+			</router-link>
+			
+			<router-link :to="{ name: 'operatorCommunications' }">
+				<button class="button">Gestion des communications</button>
+			</router-link>
 
-		<router-link :to="{ name: 'operatorCertifications' }">
-			<button>Certifications</button>
-		</router-link>
+			<router-link :to="{ name: 'operatorFormations' }">
+				<button class="button">Gestion des formations</button>
+			</router-link>
+
+			<router-link :to="{ name: 'operatorCertifications' }">
+				<button class="button">Certifications</button>
+			</router-link>
+		</div>
 	</div>
 </template>
 
@@ -34,3 +36,17 @@ export default {
 	}
 }
 </script>
+
+<style scoped>
+	#buttons {
+		text-align: center;
+	}
+
+	.button {
+		padding: 40px;
+		margin: 10px;
+		border-radius: 40px;
+		vertical-align: bottom;
+		cursor: pointer;
+	}
+</style>
