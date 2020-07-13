4896cf87599c8a9f874c232d6e2f2be5303f87f5
FrancoisBasset <>
2020-07-13 20:16:26 +0200

    test heroku

--- mainapi/index.js
+++ mainapi/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('yamljs').load('./swagger.yml');
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 const cors = require('cors');
 app.use(cors());
 
